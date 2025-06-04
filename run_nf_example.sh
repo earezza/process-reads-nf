4538af2fddcaa1b0dbdde95851d22bd0d1daddcd
earezza <>
2025-06-04 09:51:46 -0400

    added example slurm job

---
+++ run_nf_example.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=04:00:00
+#SBATCH --account=def-jdilwort
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=32
+#SBATCH --job-name=nextflow-example
+#SBATCH --output=%x-%j.out
+
+module load nextflow
+module load apptainer
+
+export NXF_DISABLE_CHECK_LATEST=true
+
+cd ~/scratch/nextflows/
+
+nextflow run process_reads.nf -c process_reads.config -profile apptainer \
+	-with-dag -with-report -with-timeline -w ~/scratch/nextflows/work-cnt/ \
+	--target CUTnTag/ \
+	--reads_type paired \
+	--assembly mm10 \
+	--length 100 \
+	--assay cutntag \
+	--multiqc_config multiqc_config.yaml \
+	--genome_index ~/projects/def-jdilwort/Reference_Files/bowtie2/Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome
